7d096bad721b53ebe87587919688ffe8d6607b3a
YudaAdiPratama <github.yuda@gmail.com>
2022-09-26 17:07:26 +0700

    commit

---
+++ CMakeModules/package.cmake
@@ -0,0 +1,11 @@
+set(VENDOR "vexanium.com")
+set(PROJECT_NAME "vex")
+set(DESC "Software for the VEXANIUM network")
+set(URL "https://github.com/vexanium/vexchain2")
+set(EMAIL "info@vexanium.com")
+
+configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_package.sh.in ${CMAKE_BINARY_DIR}/packages/generate_package.sh @ONLY)
+configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_bottle.sh ${CMAKE_BINARY_DIR}/packages/generate_bottle.sh COPYONLY)
+configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_deb.sh ${CMAKE_BINARY_DIR}/packages/generate_deb.sh COPYONLY)
+configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_rpm.sh ${CMAKE_BINARY_DIR}/packages/generate_rpm.sh COPYONLY)
+configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_tarball.sh ${CMAKE_BINARY_DIR}/packages/generate_tarball.sh COPYONLY)
